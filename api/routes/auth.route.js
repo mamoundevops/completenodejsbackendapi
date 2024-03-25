bec5afa6c672d9ae0443355ae02d97b6c8ece7b6
mamoundevops <mamoun.devops@gmail.com>
2024-03-24 23:47:29 -0700

    create sign in with google account api route and function

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { signup, signin, signOut} from '../controllers/auth.controller.js';
+import { signup, signin, signOut, google} from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
-router.get('/signout', signOut)
+router.get('/signout', signOut);
+router.post('/google', google);
  
 
 export default router;
