8eec8116d96977c1db68f136b93e7364cbdf36da
mamoundevops <mamoun.devops@gmail.com>
2024-03-24 23:40:21 -0700

    create sign up api route and function

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 
 // import routes
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 const app = express();
@@ -26,7 +27,7 @@ app.listen(process.env.PORT, () => {
 
 
 app.use('/api/user', userRouter);
-
+app.use('/api/auth', authRouter);
 
 
 
