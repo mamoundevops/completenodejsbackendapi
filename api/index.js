781e11d7aae55bbdf9c7b804642a89fb27f5f203
mamoundevops <mamoun.devops@gmail.com>
2024-03-24 23:35:50 -0700

    create a middleware( for authenitcation verification) and  functions to handle possible errors

--- api/index.js
+++ api/index.js
@@ -26,3 +26,16 @@ app.listen(process.env.PORT, () => {
 
 
 app.use('/api/user', userRouter);
+
+
+
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
