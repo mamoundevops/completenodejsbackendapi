e3a2b0da33cf25a3b92542b448f6fb75681c2b97
mamoundevops <mamoun.devops@gmail.com>
2024-03-24 23:17:24 -0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,21 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+mongoose
+  .connect('mongodb://localhost:27017/backend')
+  .then(() => {
+    console.log('Connected to MongoDB localy');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port 5000");
